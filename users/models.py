40a2050ed83fc3cd4116f3e802921fea1de43428
austenyt <austenyt@gmail.com>
2024-11-25 23:53:44 +0300

    Models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите телефон",
+    )
+    tg_nick = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Tg name",
+        help_text="Укажите ник телеграм",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
